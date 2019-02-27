422646d8519336b1bd9a8afdaf81fd755d65f415
Shilin Victor <chrono.monochrome@gmail.com>
2019-02-27 16:42:15 +0300

    janice: add OSS audio hw from i9300

--- janice.mk
+++ janice.mk
@@ -46,9 +46,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml
 
+#    $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/janice \
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/adm.sqlite-u8500:system/etc/adm.sqlite-u8500
+
+#PRODUCT_PACKAGES += \
+#    tinyplay
 
 # Use non-open-source parts if present
 $(call inherit-product-if-exists, vendor/samsung/u8500-common/janice/janice-vendor-blobs.mk)
